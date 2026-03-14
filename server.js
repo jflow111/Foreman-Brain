16ee976667f1d88d79e7b4ee5217a87bc20584fc
jflow111 <jordyn209@yahoo.com>
2026-03-13 20:37:39 -0700

    Update server.js

--- server.js
+++ server.js
@@ -51,8 +51,8 @@ app.get('/checkout', async (req, res) => {
       mode: 'subscription',
       customer_email: email,
       line_items: [{ price: PRICE_ID, quantity: 1 }],
-     success_url: FRONTEND_URL + '/auth.html',
-     cancel_url: FRONTEND_URL + '/auth.html',
+     success_url: 'https://jflow111.github.io/foreman-brain/auth.html',
+     cancel_url: 'https://jflow111.github.io/foreman-brain/auth.html',
       metadata: { supabase_uid: uid }
     });
 
